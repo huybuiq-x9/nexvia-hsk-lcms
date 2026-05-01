cd35b9e0f447eed68f4ca9da41f9ad992ce75736
Huy Bui <huybuiq.work@gmail.com>
2026-05-01 04:00:51 +0000

    update: move toast notification to top

--- front/src/contexts/ToastContext.tsx
+++ front/src/contexts/ToastContext.tsx
@@ -34,7 +34,7 @@ export function ToastProvider({ children }: { children: ReactNode }) {
   return (
     <ToastContext.Provider value={{ success, error, info }}>
       {children}
-      <div className="fixed bottom-6 right-6 z-[9999] flex flex-col gap-3 pointer-events-none">
+      <div className="fixed top-6 right-6 z-[9999] flex flex-col gap-3 pointer-events-none">
         {toasts.map(t => <ToastItem key={t.id} toast={t} onDismiss={id => setToasts(prev => prev.filter(x => x.id !== id))} />)}
       </div>
     </ToastContext.Provider>
